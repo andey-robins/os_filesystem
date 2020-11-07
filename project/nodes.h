13e16b9af2c6d51d3cb9190f208481b88f8f047b
Andey Robins <andeyrobins@gmail.com>
2020-11-07 11:47:42 -0700

    Add fnodeToBuffer function

--- project/nodes.h
+++ project/nodes.h
@@ -4,9 +4,9 @@
 #ifndef NODES_H
 #define NODES_H
 
-
 class FNode
 {
+public:
     char name;
     char type;
     int size;
@@ -18,15 +18,16 @@ class FNode
 
 class INode
 {
+public:
     int directPointers[16];
 };
 
 class DNode
 {
+public:
     int nextDirectPointer;
     FileEntry entries[10];
 };
-
 
 class FileEntry
 {
@@ -44,9 +45,10 @@ FNode createFileNode(char name, int dirAddressOne)
     {
         buffer[i] = '0';
     }
-    const char* dirAddrChars = std::to_string(dirAddressOne).c_str();
+    const char *dirAddrChars = std::to_string(dirAddressOne).c_str();
 
-    for (int i = 6; i < 10; i++){
+    for (int i = 6; i < 10; i++)
+    {
         buffer[i] = dirAddrChars[i];
     }
 
@@ -57,10 +59,55 @@ FNode createFileNode(char name, int dirAddressOne)
 }
 
 FNode loadFileNode(int blknum){};
+
+char *fileNodeToBuffer(FNode f)
+{
+    char inode[64];
+    inode[0] = f.name;
+    inode[1] = 'F';
+
+    // size
+    const char *sizeChars = std::to_string(f.size).c_str();
+    for (int i = 0; i < 4; i++)
+    {
+        inode[i + 2] = sizeChars[i];
+    }
+
+    // three direct addresses
+    for (int i = 0; i < 3; i++)
+    {
+        // direct address
+        const char *dirAddrChars = std::to_string(f.directAddress[i]).c_str();
+        for (int j = 0; j < 4; j++)
+        {
+            inode[j + 6] = dirAddrChars[j];
+        }
+    }
+
+    // date
+    for (int i = 0; i < 3; i++)
+    {
+        const char *dateByte = std::to_string(f.date[i]).c_str();
+        inode[i + 10] = dateByte[0];
+    }
+
+    // emoji
+    for (int i = 0; i < 3; i++)
+    {
+        inode[i + 13] = f.emoji[i];
+    }
+
+    // padding
+    for (int i = 16; i < 64; i++)
+    {
+        inode[i] = '0';
+    }
+
+    return inode;
+}
 DNode createDirNode(char name){};
 DNode loadDirNode(int blknum){};
 INode createIndirNode(){};
 INode loadIndirNode(int blknum){};
-
 
 #endif
